0ad0044d8fc0c0d9c1f4a799703a8c3113fbb7d6
dam-ian <dmnjonas@hotmail.co.za>
2021-02-08 21:10:45 +0000

    add files 1 and 2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * putchar - writes the character c to stdout
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main -Entry point for function to print the alphabet
+ * 10 times
+ *
+ * putchar - print variables c and n
+ */
+
+int main(void)
+{
+	int n= '\n'
+
+	for(i = 0; i < 10; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+
+		putchar(n);
+	}
+
+	return(0);
+}
